7d84deddeee42dfea3c7802aa4eaa440788a6228
Šimon Bendl <simon.bendl@student.gyarab.cz>
2024-11-21 11:05:34 +0100

    fixed zero

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-
+def fibonacci(n):
+    if n == 0:
+        return [0]
+    sequence = [0, 1]
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+    return sequence[:n]
+print(fibonacci(10))
