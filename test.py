fd7aa1dca307d29b7533871aaab8954311bf5071
Šimon Bendl <simon.bendl@student.gyarab.cz>
2024-11-21 11:14:27 +0100

    pls work

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ def fibonacci(n):
     if n == 0:
         return [0]
     sequence = [0, 1]
-    while len(sequence) < n:
-        next_number = sequence[-1] + sequence[-2]
+    for i in range(2, n):
+        next_number = sequence[i-1] + sequence[i-2]
         sequence.append(next_number)
     return sequence[:n]
 print(fibonacci(10))
