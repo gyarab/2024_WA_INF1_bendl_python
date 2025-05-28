3d4bb1cb9992958b079342f5aa79ce5b9a739e21
bendl-simon <simon.bendl@student.gyarab.cz>
2025-05-28 14:38:32 +0200

    add default for media

--- library/models.py
+++ library/models.py
@@ -16,9 +16,21 @@ class Game(models.Model):
     name  = models.CharField (max_length=100)
     genre = models.ManyToManyField(Genre)
     autors = models.ManyToManyField(Author)
-    logo      = models.ImageField(upload_to = 'logos/'  ,blank=True, null=True)
-    preview   = models.ImageField(upload_to = 'previews/',blank=True, null=True)
-    thumbnail = models.ImageField(upload_to = 'thumbnails/',blank=True, null=True)
+    logo      = models.ImageField(
+        upload_to = 'logos/'  ,
+        default = 'logos/logo_default.png',
+        blank = True, null = True,
+    )
+    preview   = models.ImageField(
+        upload_to = 'previews/',
+        default = 'previews/preview_default.png',
+        blank=True, null=True
+    )
+    thumbnail = models.ImageField(
+        upload_to = 'thumbnails/',
+        default = 'thumbnails/thumbnail_default.png',
+        blank=True, null=True)
+    
     code_path = models.CharField(blank=True, null=True)
     descript  = models.TextField(blank=True, null=True)
     favorited_by = models.ManyToManyField(User, related_name='favorite_games', blank=True)
